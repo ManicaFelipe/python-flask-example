eb4f60e88f95c6f2a039ee0cf83ee8243888900f
ManicaFelipe <felipe.manicaeng@gmail.com>
2022-11-03 21:09:29 -0300

    No ramo kubernetes/desafio-01  Mudanças a serem submetidas: 	modified:   Dockerfile 	modified:   app.py

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
+count=0
+
+
+@app.route("/health")
+def health():
+    #return str(os.environ.get("STATUS"))
+    return "OK"
+
+@app.route("/counter")
+def counter1():
+    global count
+    count += 1
+    return str(count)
 
 @app.route("/")
 def hello_world():
-    return "ola mundo"
-
+    return "olá mundo"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000, debug=True)
+    app.run(host="0.0.0.0", port=os.environ.get("APP_PORT"), debug=True)
